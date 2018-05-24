7a77428be29fe0cbc76ff335cddd04b188538ccb
Noah Naiman <nnaiman@bu.edu>
2018-05-23 22:55:36 -0600

    Removed extrenous data from Binary Tree Node.

--- Data_Structures/BinaryTreeNode.java
+++ Data_Structures/BinaryTreeNode.java
@@ -1,7 +1,7 @@
 /**
  * <h1>Binary Tree Node Class</h1>
  * A Node class used to construct a Binary tree.
- * Has one parent and two children, right and left.
+ * Has two children nodes, right and left.
  *
  * @author Noah Naiman
  * @since 5-23-2018
@@ -12,7 +12,6 @@ public class BinaryTreeNode<T extends Comparable<T>> {
     // Fields
     // **************************************************
     T data;
-    BinaryTreeNode<T> parent;
     BinaryTreeNode<T> right;
     BinaryTreeNode<T> left;
 
@@ -22,24 +21,22 @@ public class BinaryTreeNode<T extends Comparable<T>> {
 
     /**
     * Default constructor.<br>
-    * Sets data, parents, left, and right will be null.
+    * Sets data, left, and right will be null.
     */
     public BinaryTreeNode(){
     	data = null;
-    	parent = null;
     	right = null;
     	left = null;
     }
 
     /**
     * Parameterized constructor.<br>
-    * Sets parents, right, and left to null.<br>
+    * Sets right, and left to null.<br>
     * @param initialData
     *	Sets data to initialData
     */
     public BinaryTreeNode(T initialData){
     	data = initialData;
-    	parent = null;
     	right = null;
     	left = null;
     }
